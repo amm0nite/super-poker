6b68f627e1b66b2578d253f3e1ac9f312e86d1bf
amm0nite <ammonite.myfox@gmail.com>
2021-02-04 00:10:46 +0100

    Add build env

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Super Poker',
-        }),
-    ],
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
+module.exports = (env) => {
+    let mode = 'development';
+    if (env.prod) {
+        mode = 'production';
+    }
+
+    return {
+        mode,
+        entry: './src/index.js',
+        devtool: 'inline-source-map',
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                title: 'Super Poker',
+            }),
         ],
-    },
-};
+        output: {
+            filename: '[name].[contenthash].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+    }
+}
