7a92d82805e6e540185d8a5b3ed5a2545dfcc068
amm0nite <ammonite.myfox@gmail.com>
2021-02-04 00:02:45 +0100

    Remove dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         }),
     ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
