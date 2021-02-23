afa8ae0d7867db9fc3c1e46bc6315b1fb6fd0317
amm0nite <ammonite.myfox@gmail.com>
2021-02-23 23:13:34 +0100

    Add State class

--- src/index.js
+++ src/index.js
@@ -4,68 +4,11 @@ import './style.css';
 
 import WebSocketClient from './radio.js';
 import Layout from './layout.js';
-
-class Options {
-    constructor() {
-        const urlParams = new URLSearchParams(window.location.search);
-
-        this.room = urlParams.get('room');
-        this.player = null;
-        this.vote = null;
-        this.show = false;
-
-        this.others = [];
-    }
-
-    playerVote(vote) {
-        this.vote = vote;
-        if (this.show) {
-            this.show = false;
-        }
-    }
-
-    otherHello(vars) {
-        let other = this.others.find((o) => o.id === vars.id);
-        if (!other) {
-            other = {
-                id: vars.id,
-                name: vars.name,
-                vote: null,
-                show: false,
-            };
-            this.others.push(other);
-        }
-        return other;
-    }
-
-    otherVote(vars) {
-        const other = this.otherHello(vars);
-        other.vote = vars.vote;
-        if (other.show) {
-            other.show = false;
-        }
-    }
-
-    getVotes() {
-        const votes = [{ name: this.player, vote: this.vote, show: this.show }];
-        for (let other of this.others) {
-            votes.push(other);
-        }
-
-        return votes;
-    }
-
-    toggleShow(show) {
-        this.show = show;
-        for (let other of this.others) {
-            other.show = show;
-        }
-    }
-}
+import State from "./state.js";
 
 class Game {
     constructor() {
-        this.options = new Options();
+        this.state = new State();
         this.layout = new Layout();
         this.client = new WebSocketClient();
     }
@@ -84,7 +27,7 @@ class Game {
         this.client.connect();
 
         document.addEventListener('connected', (e) => {
-            this.layout.switch('options', this.options);
+            this.layout.switch('options', this.state);
         });
 
         document.addEventListener('disconnected', (e) => {
@@ -92,8 +35,8 @@ class Game {
         });
 
         document.addEventListener('enter', (e) => {
-            this.options.room = e.variables.room;
-            this.options.player = e.variables.player;
+            this.state.room = e.variables.room;
+            this.state.player = e.variables.player;
 
             this.client.selectChannel(e.variables.room);
 
@@ -101,36 +44,36 @@ class Game {
         });
 
         document.addEventListener('joined', (e) => {
-            this.layout.switch('ingame', this.options);
-            this.client.hello(this.options.player);
+            this.layout.switch('ingame', this.state);
+            this.client.hello(this.state.player);
         });
 
         document.addEventListener('choice', (e) => {
-            this.options.playerVote(e.variables.vote);
-            this.client.vote(this.options.player, this.options.vote);
-            this.layout.switch('ingame', this.options);
+            this.state.playerVote(e.variables.vote);
+            this.client.vote(this.state.player, this.state.vote);
+            this.layout.switch('ingame', this.state);
         });
 
         document.addEventListener('hello', (e) => {
-            this.options.otherHello(e.variables);
-            this.client.vote(this.options.player, this.options.vote);
-            this.layout.switch('ingame', this.options);
+            this.state.otherHello(e.variables);
+            this.client.vote(this.state.player, this.state.vote);
+            this.layout.switch('ingame', this.state);
         });
 
         document.addEventListener('vote', (e) => {
-            this.options.otherVote(e.variables);
-            this.layout.switch('ingame', this.options);
+            this.state.otherVote(e.variables);
+            this.layout.switch('ingame', this.state);
         });
 
         document.addEventListener('toggle', (e) => {
-            this.options.toggleShow(!this.options.show);
-            this.client.reveal(this.options.player, this.options.show);
-            this.layout.switch('ingame', this.options);
+            this.state.toggleShow(!this.state.show);
+            this.client.reveal(this.state.player, this.state.show);
+            this.layout.switch('ingame', this.state);
         });
 
         document.addEventListener('reveal', (e) => {
-            this.options.toggleShow(e.variables.show);
-            this.layout.switch('ingame', this.options);
+            this.state.toggleShow(e.variables.show);
+            this.layout.switch('ingame', this.state);
         });
     }
 }
---
+++ src/state.js
@@ -0,0 +1,58 @@
+
+export default class State {
+    constructor() {
+        const urlParams = new URLSearchParams(window.location.search);
+
+        this.room = urlParams.get('room');
+        this.player = null;
+        this.vote = null;
+        this.show = false;
+
+        this.others = [];
+    }
+
+    playerVote(vote) {
+        this.vote = vote;
+        if (this.show) {
+            this.show = false;
+        }
+    }
+
+    otherHello(vars) {
+        let other = this.others.find((o) => o.id === vars.id);
+        if (!other) {
+            other = {
+                id: vars.id,
+                name: vars.name,
+                vote: null,
+                show: false,
+            };
+            this.others.push(other);
+        }
+        return other;
+    }
+
+    otherVote(vars) {
+        const other = this.otherHello(vars);
+        other.vote = vars.vote;
+        if (other.show) {
+            other.show = false;
+        }
+    }
+
+    getVotes() {
+        const votes = [{ name: this.player, vote: this.vote, show: this.show }];
+        for (let other of this.others) {
+            votes.push(other);
+        }
+
+        return votes;
+    }
+
+    toggleShow(show) {
+        this.show = show;
+        for (let other of this.others) {
+            other.show = show;
+        }
+    }
+}
